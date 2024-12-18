ffdcaa4592c1338cf4e35202b39730e9c4cc2fac
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2024-12-18 16:12:26 +0000

    ejercicios 1 2 3

---
+++ practicas/practica1/p1.php
@@ -0,0 +1,68 @@
+<?php
+class Cotxe {
+    // Atributos
+    private $marca;
+    private $model;
+
+    // Contructor para inicalizar los atributos con valores por defecto
+    public function __construct($marca = "Desconeguda", $model = "Desconegut") {
+        $this->marca = $marca;
+        $this->model = $model;
+    }
+
+    // metodo para obtener la descripcion del coche
+    public function descripcio() {
+        return "El cotxe és de la marca " . $this->marca . " i el model és " . $this->model . ".";
+    }
+}
+
+
+
+
+// Ejemplos
+$cotxe1 = new Cotxe(); 
+echo $cotxe1->descripcio(); 
+
+$cotxe2 = new Cotxe("Ford", "Focus"); 
+echo $cotxe2->descripcio(); 
+
+
+class Persona {
+    // Atributos
+    private $nom;
+    private $edat;
+
+    // Constructor que inicializa los atributos
+    public function __construct($nom, $edat) {
+        $this->nom = $nom;
+        $this->edat = $edat;
+    }
+
+    // metodo para mostrar un mensaje de bienvenida
+    public function benvinguda() {
+        return "Benvingut/a, " . $this->nom . "! Tens " . $this->edat . " anys.";
+    }
+}
+
+// Ejemplo
+$persona1 = new Persona("Joan", 30); // Crea un objeto de la clase Persona
+echo $persona1->benvinguda(); 
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><h1>holas</h1>
+    
+</body>
+</html>
